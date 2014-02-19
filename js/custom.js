077cd2af459fa9232effffdce3558c9a97230ac8
Raoul D'Cunha <raoul_dcunha@hotmail.com>
2014-02-19 08:49:42 +0000

    changed scrollbar colour to red from black and disabled scrollbar autohide

--- js/custom.js
+++ js/custom.js
@@ -20,38 +20,12 @@
 //custom scrollbar
 	$(document).ready(
 	function() {  
-		$("html").niceScroll();
+		$("html").niceScroll({
+        cursorcolor: "#d84a38",
+        autohidemode: false,
+    });
 		}
 	);
-
-//contact form
-	$(document).ready(function() {
-	var options = {
-	target: '.alert',
-	beforeSubmit: showRequest,
-	success: showResponse
-	};
-	$('#contactForm').ajaxForm(options);
-	});
-	function showRequest(formData, jqForm, options) {
-	var queryString = $.param(formData);
-	return true;
-	}
-	function showResponse(responseText, statusText)  {
-	}
-	$.fn.clearForm = function() {
-		return this.each(function() {
-		var type = this.type, tag = this.tagName.toLowerCase();
-		if (tag == 'form')
-			return $(':input',this).clearForm();
-		if (type == 'text' || type == 'password' || tag == 'textarea')
-			this.value = '';
-		else if (type == 'checkbox' || type == 'radio')
-			this.checked = false;
-		else if (tag == 'select')
-			this.selectedIndex = -1;
-		});
-	};
 
 //smooth scroll on page
 	$(function() {
